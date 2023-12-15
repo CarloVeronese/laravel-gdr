f76534d21016bbfc313b8e3438f888f287aefd0e
Limson91 <matteo.a.colautti@gmail.com>
2023-12-15 15:47:50 +0100

    add - feature/characters-migration

---
+++ database/migrations/2023_12_15_143223_create_characters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('bio');
+            $table->tinyInteger('defense');//->unsigned(); possono essere valori negativi?
+            $table->tinyInteger('speed'); 
+            $table->tinyInteger('hp');
+            $table->tinyInteger('strength');
+            $table->string('class', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characters');
+    }
+};
