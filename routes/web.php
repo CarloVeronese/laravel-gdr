a2b9f0ff8d1e77a74ecac1bd2fd2db610309169d
CarloVeronese <carlo.veronese97@gmail.com>
2023-12-22 16:19:28 +0100

    add - auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,26 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::get('/', [CharacterController::class, 'index'])->name('home');
+Route::get('/characters', [CharacterController::class, 'index'])->name('characters.index');
+Route::get('/characters/create', [CharacterController::class, 'create'])->name('characters.create');
+Route::get('/characters/{character}', [CharacterController::class, 'show'])->name('characters.show');
+Route::post('/characters', [CharacterController::class, 'store'])->name('characters.store');
+Route::get('/characters/{character}/edit', [CharacterController::class, 'edit'])->name('characters.edit');
+Route::put('/characters/{character}',[CharacterController::class, 'update'])->name('characters.update');
+Route::delete('/characters/{character}',[CharacterController::class, 'destroy'])->name('characters.destroy');
 
-Route::get('/characters', [CharacterController::class, 'index'])
-  ->name('characters.index');
-
-Route::get('/characters/create', [CharacterController::class, 'create'])
-  ->name('characters.create');
-
-Route::get('/characters/{character}', [CharacterController::class, 'show'])
-  ->name('characters.show');
-
-Route::post('/characters', [CharacterController::class, 'store'])
-  ->name('characters.store');
-
-Route::get('/characters/{character}/edit', [CharacterController::class, 'edit'])
-  ->name('characters.edit');
-
-Route::put('/characters/{character}',[CharacterController::class, 'update'])
-  ->name('characters.update');
-
-  
-Route::delete('/characters/{character}',[CharacterController::class, 'destroy'])
-->name('characters.destroy');
+require __DIR__.'/auth.php';
